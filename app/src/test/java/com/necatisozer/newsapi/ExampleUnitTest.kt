3df9f629b11fe1e87d29c3111e96719a4abb5b4b
necati <necatisozer@gmail.com>
2019-07-03 01:40:10 +0300

    Update package name

--- app/src/test/java/com/necatisozer/newsapi/ExampleUnitTest.kt
+++ app/src/test/java/com/necatisozer/newsapi/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.example.newsapi
+package com.necatisozer.newsapi
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
