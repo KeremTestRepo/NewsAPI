1c26df1686e5ab4d78719a1595a3636301cf4cb3
necati <necatisozer@gmail.com>
2019-07-04 11:32:52 +0300

    Remove Response wrapper

--- data/src/main/java/com/necatisozer/data/api/NewsApi.kt
+++ data/src/main/java/com/necatisozer/data/api/NewsApi.kt
@@ -3,16 +3,15 @@ package com.necatisozer.data.api
 import com.necatisozer.data.api.entity.Article
 import com.necatisozer.data.api.entity.Source
 import com.serjltt.moshi.adapters.Wrapped
-import retrofit2.Response
 import retrofit2.http.GET
 
 interface NewsApi {
     @GET("sources")
     @Wrapped(path = ["sources"])
-    suspend fun getSources(): Response<List<Source>>
+    suspend fun getSources(): List<Source>
 
     @GET("top-headlines")
     @Wrapped(path = ["articles"])
-    suspend fun getTopHeadlines(): Response<List<Article>>
+    suspend fun getTopHeadlines(): List<Article>
 
 }
---
+++ data/src/main/java/com/necatisozer/data/extension/NetworkRequest.kt
@@ -0,0 +1,20 @@
+package com.necatisozer.data.extension
+
+import com.necatisozer.common.exception.NetworkError
+import com.necatisozer.common.exception.ServerError
+import com.necatisozer.common.helper.Result
+import retrofit2.HttpException
+import java.io.IOException
+
+suspend inline fun <reified T> networkRequest(crossinline request: suspend () -> T): Result<T> {
+    return try {
+        Result.success(request())
+    } catch (exception: HttpException) {
+        Result.failure(ServerError(code = exception.code(), message = exception.message()))
+    } catch (exception: IOException) {
+        Result.failure(NetworkError())
+    } catch (exception: Exception) {
+        Result.failure(exception)
+    }
+}
+
