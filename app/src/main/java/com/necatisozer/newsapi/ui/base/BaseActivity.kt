052cac4329388fe0052bb0eb82ee6c5c65681a32
necati <necatisozer@gmail.com>
2019-07-05 12:51:39 +0300

    Add random dark mode for demo purposes

--- app/src/main/java/com/necatisozer/newsapi/ui/base/BaseActivity.kt
+++ app/src/main/java/com/necatisozer/newsapi/ui/base/BaseActivity.kt
@@ -1,5 +1,19 @@
 package com.necatisozer.newsapi.ui.base
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import org.threeten.bp.Instant
+import kotlin.random.Random
 
-abstract class BaseActivity : AppCompatActivity()
+abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Just for demo... will be added to menu
+        delegate.localNightMode = when (Random(Instant.now().toEpochMilli()).nextBoolean()) {
+            true -> AppCompatDelegate.MODE_NIGHT_YES
+            false -> AppCompatDelegate.MODE_NIGHT_NO
+        }
+    }
+}
